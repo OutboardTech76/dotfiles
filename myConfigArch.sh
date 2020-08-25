69d467f34d2526f14dcb8b922d902803af69a0cc
OutboardTech76 <fme11@protonmail.com>
2020-08-25 20:47:26 +0200

    Configuration script for arch installation

---
+++ myConfigArch.sh
@@ -0,0 +1,30 @@
+# !/bin/bash
+# Needed files
+sudo pacman -S autoconf automake base binutils bison cmake exa fakeroot feh firefox flex gcc git gtk-chtheme i3-gaps intel-ucode lxappearance m4 make neovim networkmanager nvidia openssh patch pcmanfm pkgconf pulseaudio pulseaudio-alsa python-pip python-pynvim python2 python2-pip ranger refind scrot stow termite texinfo thunderbird vim xclip xorg zathura zathura-djvu zathura-pdf-mupdf zsh  
+ 
+#Necessary files nevim
+pip install pynvim 
+pip2 install pynvim 
+
+#Softlink everything with stow 
+stow --target=$HOME/.config -v -R dot-config 
+stow  --dotfiles -R -v zsh-config
+stow  --dotfiles -R -v Xfiles
+sudo stow --target=/etc/X11/xorg.conf.d  -v -R xorg.conf.d
+
+## Install dmenu
+cd dmenu
+sudo make clean install
+cd ..
+
+# Install yay
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+cd ..
+sudo rm -r yay
+
+# Install yay apps
+yay -S --noconfirm polybar tdrop-git picom-rounded-corners
+
+
