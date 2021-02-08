762fd4edf7ceb99b2d0948e32002ff3517334cde
OutboardTech76 <fme11@protonmail.com>
2021-02-08 20:55:47 +0100

    Updated. Now sets zsh as default shell

--- myConfigArch.sh
+++ myConfigArch.sh
@@ -1,12 +1,16 @@
 # !/bin/bash
+# Update PGP keys before installing anything to avoid possible errors when importing them
+sudo pacman -Syu archlinux-keyring
+sudo pacman -Syu
 # Needed files
-sudo pacman -S autoconf automake base binutils bison cmake exa fakeroot feh firefox flex gcc git gtk-chtheme i3-gaps intel-ucode lxappearance m4 make neovim networkmanager nvidia openssh patch pcmanfm pkgconf pulseaudio pulseaudio-alsa python-pip python-pynvim python2 python2-pip ranger refind scrot stow termite texinfo thunderbird vim xclip xorg zathura zathura-djvu zathura-pdf-mupdf zsh w3m
+sudo pacman -S autoconf automake base binutils bison cmake exa fakeroot feh firefox flex gcc git gtk-chtheme i3-gaps intel-ucode lxappearance m4 make neovim nvidia openssh patch pcmanfm pkgconf pulseaudio pulseaudio-alsa python-pip python-pynvim python2 python2-pip ranger scrot stow termite texinfo thunderbird xclip xorg xorg-xinit zathura zathura-djvu zathura-pdf-mupdf zsh w3m npm man-db man-pages
  
 #Necessary files nevim
 pip install pynvim 
 pip2 install pynvim 
 
 #Softlink everything with stow 
+mkdir -p $HOME/.config
 stow --target=$HOME/.config -v -R dot-config 
 stow  --dotfiles -R -v zsh-config
 stow  --dotfiles -R -v Xfiles
@@ -23,9 +27,13 @@ git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
 cd ..
-sudo rm -r yay
+rm -rf yay
 
 # Install yay apps
 yay -S --noconfirm polybar tdrop-git picom-rounded-corners
 
+# Set zsh as default shell for user and su
+# Requires password
+chsh -s $(which zsh)
+sudo chsh -s $(which zsh)
 
