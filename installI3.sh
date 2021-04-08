1ca557cc282c9451be45b10b77d7959dec9cde7a
OutboardTech76 <fme11@protonmail.com>
2021-04-09 01:23:15 +0200

    Updated

--- installI3.sh
+++ installI3.sh
@@ -4,10 +4,14 @@ sudo apt-get install -y libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev \
 	libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libxcb-xkb-dev \
 	libstartup-notification0-dev libxcb-randr0-dev libxkbcommon-x11-0 libxkbcommon-x11-dev \
 	libev-dev libxcb-cursor-dev libxcb-xinerama0-dev \
-	autoconf libxcb-xrm0 libxcb-xrm-dev automake libxcb-shape0-dev
+	autoconf libxcb-xrm0 libxcb-xrm-dev automake libxcb-shape0-dev \
+	meson ninja-build
 
+mkdir -p $HOME/installations
+cd $HOME/installations
 git clone https://www.github.com/Airblader/i3 i3-gaps
 cd i3-gaps
 mkdir -p build && cd build
 meson ..
 ninja
+sudo ninja install
--- installTermite.sh
+++ installTermite.sh
@@ -1,25 +1,55 @@
 # !/bin/bash
 
-# Clone termite and vte repos
-git clone --recursive https://github.com/thestinger/termite.git termite
-git clone https://github.com/thestinger/vte-ng.git vte
+# Copied from  https://github.com/ls4154/termite-ubuntu.git
+ 
+mkdir -p $HOME/installations
+cd $HOME/installations
 
-# Install vte dependencies
-sudo apt-get install -y autoconf libglib2.0-dev gtk-doc-tools intltool \
-	gperf libgnutls28-dev gnutls-bin libxml2-utils libgtk-3-dev \
-	libpcre2-dev libglib3.0-cil-dev libgirepository1.0-dev \
-	valac
+sudo apt-get install -y g++ libgtk-3-dev gtk-doc-tools gnutls-bin \
+    valac libtool intltool libpcre2-dev libglib3.0-cil-dev \
+    libgnutls28-dev libgirepository1.0-dev libxml2-utils \
+    gperf g++
+ 
+# Clone termite and vte repos
+git clone --recursive -b v15 https://github.com/thestinger/termite.git termite
+git clone -b 0.56.2 https://github.com/thestinger/vte-ng.git vte
 
 
 # Install vte
+cd vte
+NOCONFIGURE ./autogen.sh
+./configure \
+    --prefix=$HOME/installations/vte-static \
+    --enable-static \
+    --disable-shared \
+    --enable-vala=no \
+    --disable-gtk-doc \
+    --disable-introspection
 
-# cd vte
-# ./autogen.sh --disable-introspection --disable-vala
-# ./configure --disable-instrospection --disable-vala
-# make -j8
-# sudo make install -j8
+make -j8
+make install -j8
+make install-pkgconfigDATA -j8
 
-# Install termite
-# cd ../termite
-# make -j8
-# sudo make install -j8
+## Install termite
+cd $HOME/installations/termite
+export PKG_CONFIG_PATH="$HOME/installations/vte-static/lib/pkgconfig"
+make -j8
+sudo make install -j8
+ 
+sudo cat << EOF > termite.wrapper
+#!/bin/bash
+exec /usr/local/bin/termite "\$@"
+EOF
+
+sudo chmod +x termite.wrapper
+
+sudo install -d /usr/local/bin/
+sudo install -m 755 termite.wrapper /usr/local/bin/
+
+sudo update-alternatives --install /usr/bin/x-terminal-emulator x-terminal-emulator /usr/local/bin/termite.wrapper 60
+
+sudo install -d /usr/local/etc/profile.d/
+sudo install -m 644 "$HOME/installations/vte-static/etc/profile.d/vte.sh" /usr/local/etc/profile.d/
+
+sudo install -d /usr/share/terminfo/x
+sudo ln -s /usr/local/share/terminfo/x/xterm-termite /usr/share/terminfo/x/xterm-termite
