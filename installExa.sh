5c16761607d87ec1233d78aafe575fc689def14a
OutboardTech76 <fme11@protonmail.com>
2021-04-09 01:23:52 +0200

    Scripts for installing configuration in Ubuntu

---
+++ installExa.sh
@@ -0,0 +1,13 @@
+# !/bin/bash
+
+mkdir -p $HOME/installations/exa
+cd  $HOME/installations/exa
+
+# Download exa and extract
+wget -c https://github.com/ogham/exa/releases/download/v0.10.0/exa-linux-x86_64-v0.10.0.zip -O exa.zip
+unzip exa.zip
+
+# Move downloaded files
+sudo mv bin/exa /usr/local/bin
+sudo mv man/* /usr/share/man/man1
+sudo mv completions/exa.zsh /usr/local/share/zsh/site-functions
