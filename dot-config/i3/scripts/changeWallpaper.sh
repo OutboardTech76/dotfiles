053ba018f1e04d7f7876ef8f8ab9e4268732beb8
OutboardTech76 <fme11@protonmail.com>
2021-05-07 18:18:52 +0200

    Delayed all times 2 hours

--- dot-config/i3/scripts/changeWallpaper.sh
+++ dot-config/i3/scripts/changeWallpaper.sh
@@ -4,42 +4,41 @@ sleep 1
 #exit
 while true; do
    date=$(date +%H | sed 's/\<0//g')
-if (($date==$olddate)); then
-   echo "nothing to do"
+if (($date==$olddate)); then :
 else
-   if (($date>= 22));then
+   if (($date>= 24));then
        feh --bg-scale $HOME/.config/i3/wallpapers/mojave_dynamic_16.jpeg
-   elif (($date >= 21));then
+   elif (($date >= 23));then
        feh --bg-scale $HOME/.config/i3/wallpapers/mojave_dynamic_15.jpeg
-   elif (($date >= 20));then
+   elif (($date >= 22));then
        feh --bg-scale $HOME/.config/i3/wallpapers/mojave_dynamic_14.jpeg
-   elif (($date >= 19));then
+   elif (($date >= 21));then
        feh --bg-scale $HOME/.config/i3/wallpapers/mojave_dynamic_13.jpeg
-   elif (($date >= 18));then
+   elif (($date >= 20));then
        feh --bg-scale $HOME/.config/i3/wallpapers/mojave_dynamic_12.jpeg
+   elif (($date >= 19));then
+       feh --bg-scale $HOME/.config/i3/wallpapers/mojave_dynamic_11.jpeg
+   elif (($date >= 18));then
+       feh --bg-scale $HOME/.config/i3/wallpapers/mojave_dynamic_11.jpeg
    elif (($date >= 17));then
-       feh --bg-scale $HOME/.config/i3/wallpapers/mojave_dynamic_11.jpeg
-   elif (($date >= 16));then
-       feh --bg-scale $HOME/.config/i3/wallpapers/mojave_dynamic_11.jpeg
-   elif (($date >= 15));then
        feh --bg-scale $HOME/.config/i3/wallpapers/mojave_dynamic_10.jpeg
-   elif (($date >= 14));then
+   elif (($date >= 16));then
        feh --bg-scale $HOME/.config/i3/wallpapers/mojave_dynamic_9.jpeg
-   elif (($date >= 13));then
+   elif (($date >= 15));then
        feh --bg-scale $HOME/.config/i3/wallpapers/mojave_dynamic_8.jpeg
-   elif (($date >= 12));then
+   elif (($date >= 14));then
        feh --bg-scale $HOME/.config/i3/wallpapers/mojave_dynamic_7.jpeg
-   elif (($date >= 11));then
+   elif (($date >= 13));then
        feh --bg-scale $HOME/.config/i3/wallpapers/mojave_dynamic_6.jpeg
-   elif (($date >= 9));then
+   elif (($date >= 11));then
        feh --bg-scale $HOME/.config/i3/wallpapers/mojave_dynamic_5.jpeg
-   elif (($date >= 8));then
+   elif (($date >= 10));then
        feh --bg-scale $HOME/.config/i3/wallpapers/mojave_dynamic_4.jpeg
-   elif (($date >= 7));then
+   elif (($date >= 9));then
        feh --bg-scale $HOME/.config/i3/wallpapers/mojave_dynamic_3.jpeg
-   elif (($date >= 6));then
+   elif (($date >= 8));then
        feh --bg-scale $HOME/.config/i3/wallpapers/mojave_dynamic_2.jpeg
-   elif (($date >= 5));then
+   elif (($date >= 7));then
        feh --bg-scale $HOME/.config/i3/wallpapers/mojave_dynamic_1.jpeg
     else
        feh --bg-scale $HOME/.config/i3/wallpapers/mojave_dynamic_16.jpeg
